d7f13a4757b15511f13749d9357f537445b525b9
Oma-yma1 <zgani.omayma@gmail.com>
2024-01-03 16:29:46 +0000

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true; // Update the existing variables without redeclaration
+    task2 = false; // Update the existiong variables without redeclaration
   }
 
   return [task, task2];
